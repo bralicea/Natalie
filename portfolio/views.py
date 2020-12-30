c6a003503199397b61acaad9b9c8452749a0ed6e
bralicea <aliceabryan24@gmail.com>
2020-12-30 17:44:36 -0500

    finished implementing about model

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from .models import About
 
 def index(request):
-    return TemplateResponse(request, 'index.html')
+    about = About.objects.get()
+    return TemplateResponse(request, 'index.html', {"about": about})
